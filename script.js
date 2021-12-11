75f9d6326870339e041aef6e63c88f6747d710e0
ELIJAH KIRAGU <eliki13720@gmail.com>
2021-12-11 13:11:16 +0300

    added reset button with the label CANCEL

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ femalenames =["Akosu", "Adwoa", "Abenaa", "Akuu", "Yaa", "Afua", "Ama"]
 
 
 function dayCalculator (){
-    Day_of_the_week = ( ( (19/4) -2*19-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7
+    var =
+    Day_of_the_week = (((19/4) -2*19-1) + ((5*parseInt(year)/4) ) + ((26*(parseInt(month)+1)/10)) + parseInt(date)) % 7
     return Day_of_the_week
 }
-alert (Day_of_the_week)
+var answer = dayCalculator
+console.log(answer)
